57b5e1d35b6e34dc5a13b1f97daaf01a193dd372
Daniel Nykamp <djnykamp@gmail.com>
2022-06-17 12:34:51 -0500

    other files

---
+++ target/wasm32-unknown-unknown/release/DoenetRustWASM.d
@@ -0,0 +1,1 @@
+/home/daniel/MyCode/DoenetRustWASM/target/wasm32-unknown-unknown/release/DoenetRustWASM.wasm: /home/daniel/MyCode/DoenetRustWASM/src/lib.rs /home/daniel/MyCode/DoenetRustWASM/src/utils.rs
