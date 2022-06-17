ef49f5f0f3f5fb13aed33fcc5ac8b98c715f468d
Daniel Nykamp <djnykamp@gmail.com>
2022-06-17 12:23:03 -0500

    fix www folder

---
+++ www/index.js
@@ -0,0 +1,18 @@
+import { DoenetCore } from "wasm-json-test"
+import { parseAndCompile } from "./Parser/parser.js"
+
+
+const DoenetText = "<text><text>hello</text> world</text>";
+// const DoenetText = "<answer><award><math>x</math></award></answer>";
+// const DoenetText = "<answer symbolicEquality><award><math>x</math></award></answer>";
+
+const DoenetTextJson = parseAndCompile(DoenetText);
+
+console.log("JSON", DoenetTextJson);
+
+const dc = DoenetCore.new(JSON.stringify(DoenetTextJson));
+
+const render_tree_string = dc.render_tree();
+const render_tree_json = JSON.parse(render_tree_string);
+
+console.log("JS deserialized JSON", render_tree_json);
