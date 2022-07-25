f0ad67a447a168dbd47e53e02dd9170da54ac177
Daniel <djnykamp@gmail.com>
2022-07-25 09:43:52 -0500

    add new untracked files

---
+++ core/src/component/document.rs
@@ -0,0 +1,341 @@
+use std::collections::HashMap;
+
+use core_derive::ComponentLike;
+
+use lazy_static::lazy_static;
+
+use crate::prelude::*;
+use crate::state_variables::*;
+use super::*;
+
+use crate::{ObjectTraitName, ComponentLike,
+ComponentSpecificBehavior, ComponentChild};
+
+use crate::state_var::{StateVar, EssentialStateVar};
+
+
+
+#[derive(Debug, ComponentLike)]
+pub struct Document {
+    name: String,
+    parent: Option<String>,
+    children: Vec<ComponentChild>,
+
+    // Note that this is not behind a RefCell, so we can't change the hashmap
+    // once the component is created
+    essential_state_vars: HashMap<StateVarName, EssentialStateVar>,
+
+    attributes: HashMap<AttributeName, Attribute>,
+
+    copy_target: Option<String>,
+
+    // State variables
+    submit_label: StateVar,
+    submit_label_no_correctness: StateVar,
+    hidden: StateVar,
+    disabled: StateVar,
+    fixed: StateVar,
+    // title_child_name: StateVar,
+    title: StateVar,
+    level: StateVar,
+    just_submitted: StateVar,
+    show_correctness: StateVar,
+    credit_achieved: StateVar,
+    create_submit_all_button: StateVar,
+    suppress_answer_submit_buttons: StateVar,
+
+}
+
+
+
+#[derive(Debug)]
+struct MyStateVars {
+    submit_label: StateVar,
+    submit_label_no_correctness: StateVar,
+    hidden: StateVar,
+    disabled: StateVar,
+    fixed: StateVar,
+    // title_child_name: StateVar,
+    title: StateVar,
+    level: StateVar,
+    just_submitted: StateVar,
+    show_correctness: StateVar,
+    credit_achieved: StateVar,
+    create_submit_all_button: StateVar,
+    suppress_answer_submit_buttons: StateVar,
+
+    essential_state_vars: HashMap<StateVarName, EssentialStateVar>,
+
+}
+
+impl ComponentStateVars for MyStateVars {
+    fn get(&self, state_var_name: StateVarName) -> Result<&StateVar, String> {
+        match state_var_name {
+            "submitLabel" => Ok(&self.submit_label),
+            "submitLabelNoCorrectness" => Ok(&self.submit_label_no_correctness),
+            "hidden" => Ok(&self.hidden),
+            "disabled" => Ok(&self.disabled),
+            "fixed" => Ok(&self.fixed),
+            "title" => Ok(&self.title),
+            "level" => Ok(&self.level),
+            "justSubmitted" => Ok(&self.just_submitted),
+            "showCorrectness" => Ok(&self.show_correctness),
+            "creditAchieved" => Ok(&self.credit_achieved),
+            "createSubmitAllButton" => Ok(&self.create_submit_all_button),
+            "suppressAnswerSubmitButtons" => Ok(&self.suppress_answer_submit_buttons),
+
+            _ => Err(format!("Document does not have state var {}", state_var_name))
+        }
+    }
+
+    fn get_essential_state_vars(&self) -> &HashMap<StateVarName, EssentialStateVar> {
+        &self.essential_state_vars
+    }
+}
+
+
+
+
+#[derive(Debug, Default, Clone)]
+struct MyAttributeData {
+
+    // These types could be more specific
+    hide: Option<Attribute>,
+    disabled: Option<Attribute>,
+}
+
+impl AttributeData for MyAttributeData {
+    fn add_attribute(&mut self, name: AttributeName, attribute: Attribute) -> Result<(), String> {
+        match name {
+            "hide" => {
+                self.hide = Some(attribute);
+            },
+            "disabled" => {
+                self.disabled = Some(attribute);
+            },
+
+            _ => {
+                return Err("Invalid attribute name".to_string())
+            }
+        }
+        Ok(())
+    }
+
+    fn get(&self, name: AttributeName) -> &Option<Attribute> {
+        match name {
+            "hide" => &self.hide,
+            "disabled" => &self.disabled,
+            _ => panic!("Invalid attribute name {} for text", name)
+        }
+    }
+}
+
+
+
+lazy_static! {
+
+    pub static ref MY_STATE_VAR_DEFINITIONS: HashMap<StateVarName, StateVarVariant> = {
+        use StateVarUpdateInstruction::*;
+
+        let mut state_var_definitions = HashMap::new();
+        
+        state_var_definitions.insert("submitLabel", StateVarVariant::String(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue("Check Work".to_string()),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("submitLabelNoCorrectness", StateVarVariant::String(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue("Submit Response".to_string()),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("hidden", StateVarVariant::Boolean(Default::default()));
+
+        state_var_definitions.insert("disabled", DISABLED_DEFAULT_DEFINITION());
+
+        state_var_definitions.insert("fixed", FIXED_DEFAULT_DEFINITION());
+
+        // state_var_definitions.insert("titleChildName", StateVarVariant::String(StateVarDefinition {
+        //     determine_state_var_from_dependencies: |_| SetValue("Submit Response"),
+        //     for_renderer: true,
+        //     ..Default::default()
+        // })); 
+
+        
+        state_var_definitions.insert("title", StateVarVariant::String(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue("".to_string()),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("level", StateVarVariant::Number(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(0.0),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("justSubmitted", StateVarVariant::Boolean(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(true),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("showCorrectness", StateVarVariant::Boolean(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(true),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("creditAchieved", StateVarVariant::Number(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(1.0),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("createSubmitAllButton", StateVarVariant::Boolean(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(false),
+            for_renderer: true,
+            ..Default::default()
+        }));
+
+
+        state_var_definitions.insert("suppressAnswerSubmitButtons", StateVarVariant::Boolean(StateVarDefinition {
+            determine_state_var_from_dependencies: |_| SetValue(false),
+            for_renderer: true,
+            ..Default::default()
+        }));        
+
+        
+
+
+
+
+        return state_var_definitions
+    };
+}
+
+
+lazy_static! {
+    pub static ref MY_ATTRIBUTE_DEFINITIONS: HashMap<AttributeName, AttributeDefinition> = {
+        let mut attribute_definitions = HashMap::new();
+
+        attribute_definitions.insert("hide", AttributeDefinition::Component("boolean"));
+
+        attribute_definitions
+    };
+}
+
+
+impl ComponentSpecificBehavior for Document {
+
+    fn state_variable_instructions(&self) -> &'static HashMap<StateVarName, StateVarVariant> {
+        &MY_STATE_VAR_DEFINITIONS
+    }
+
+    fn attribute_instructions(&self) -> &'static HashMap<AttributeName, AttributeDefinition> {
+        &MY_ATTRIBUTE_DEFINITIONS
+    }
+
+    fn attributes(&self) -> &HashMap<AttributeName, Attribute> {
+        &self.attributes
+    }
+
+    fn should_render_children(&self) -> bool { true }
+
+    fn get_trait_names(&self) -> Vec<ObjectTraitName> {
+        vec![]
+    }
+
+    fn action_names(&self) -> Vec<&'static str> { vec![] }
+
+
+    fn get_copy_target_if_exists(&self) -> &Option<String> {
+        &self.copy_target
+    }
+
+
+}
+
+
+#[derive(Clone)]
+pub struct MyComponentDefinition;
+
+impl ComponentDefinition for MyComponentDefinition {
+    fn attribute_definitions(&self) -> &'static HashMap<AttributeName, AttributeDefinition> {
+        &MY_ATTRIBUTE_DEFINITIONS
+    }
+
+    fn state_var_definitions(&self) -> &'static HashMap<StateVarName, StateVarVariant> {
+        &MY_STATE_VAR_DEFINITIONS
+    }
+
+    fn get_trait_names(&self) -> Vec<ObjectTraitName> {
+        vec![]
+    }
+
+    fn should_render_children(&self) -> bool {
+        true
+    }
+
+
+
+    fn empty_attribute_data(&self) -> Box<dyn AttributeData> {
+        Box::new(MyAttributeData { ..Default::default() })
+    }
+
+    fn new_stale_component_state_vars(&self) -> Box<dyn ComponentStateVars> {
+        Box::new(MyStateVars {
+            submit_label: StateVar::new(StateVarValueType::String),
+            submit_label_no_correctness: StateVar::new(StateVarValueType::String),
+            hidden: StateVar::new(StateVarValueType::Boolean),
+            disabled: StateVar::new(StateVarValueType::Boolean),
+            fixed: StateVar::new(StateVarValueType::Boolean),
+            // title_child_name: StateVar::new(StateVarValueType::),
+            title: StateVar::new(StateVarValueType::String),
+            level: StateVar::new(StateVarValueType::Number),
+            just_submitted: StateVar::new(StateVarValueType::Boolean),
+            show_correctness: StateVar::new(StateVarValueType::Boolean),
+            credit_achieved: StateVar::new(StateVarValueType::Number),
+            create_submit_all_button: StateVar::new(StateVarValueType::Boolean),
+            suppress_answer_submit_buttons: StateVar::new(StateVarValueType::Boolean), 
+
+            essential_state_vars: HashMap::new(),
+        })
+    }
+
+
+}
+
+
+
+impl Document {
+    pub fn create(name: String, parent: Option<String>, children: Vec<ComponentChild>, essential_state_vars: HashMap<StateVarName, EssentialStateVar>, attributes: HashMap<AttributeName, Attribute>, copy_target: Option<String>,
+    
+    ) -> Box<dyn ComponentLike> {
+        Box::new(Document {
+            name,
+            parent,
+            children,
+
+            essential_state_vars,
+            attributes,
+            copy_target,
+            
+            submit_label: StateVar::new(StateVarValueType::String),
+            submit_label_no_correctness: StateVar::new(StateVarValueType::String),
+            hidden: StateVar::new(StateVarValueType::Boolean),
+            disabled: StateVar::new(StateVarValueType::Boolean),
+            fixed: StateVar::new(StateVarValueType::Boolean),
+            // title_child_name: StateVar::new(StateVarValueType::),
+            title: StateVar::new(StateVarValueType::String),
+            level: StateVar::new(StateVarValueType::Number),
+            just_submitted: StateVar::new(StateVarValueType::Boolean),
+            show_correctness: StateVar::new(StateVarValueType::Boolean),
+            credit_achieved: StateVar::new(StateVarValueType::Number),
+            create_submit_all_button: StateVar::new(StateVarValueType::Boolean),
+            suppress_answer_submit_buttons: StateVar::new(StateVarValueType::Boolean),
+        })
+    }
+}
---
+++ core/src/component/number.rs
@@ -0,0 +1,150 @@
+use std::collections::HashMap;
+
+use core_derive::ComponentLike;
+
+use lazy_static::lazy_static;
+
+use crate::prelude::*;
+use crate::state_variables::*;
+use super::*;
+
+use crate::{ObjectTraitName, ComponentLike,
+ComponentSpecificBehavior, ComponentChild};
+
+use crate::state_var::{StateVar, EssentialStateVar};
+
+
+
+
+#[derive(Debug, ComponentLike)]
+pub struct Number {
+    name: String,
+    parent: Option<String>,
+    children: Vec<ComponentChild>,
+
+    // Note that this is not behind a RefCell, so we can't change the hashmap
+    // once the component is created
+    essential_state_vars: HashMap<StateVarName, EssentialStateVar>,
+
+    attributes: HashMap<AttributeName, Attribute>,
+
+    copy_target: Option<String>,
+
+    // State variables
+    value: StateVar,
+    hidden: StateVar,
+}
+
+
+lazy_static! {
+    pub static ref MY_STATE_VAR_DEFINITIONS: HashMap<StateVarName, StateVarVariant> = {
+        use StateVarUpdateInstruction::*;
+
+        let mut state_var_definitions = HashMap::new();
+        
+        state_var_definitions.insert("value", StateVarVariant::Number(StateVarDefinition {
+            for_renderer: true,
+
+            return_dependency_instructions: |_| {
+                let instruction = DependencyInstruction::Child(ChildDependencyInstruction {
+                    desired_children: vec![ObjectTraitName::NumberLike],
+                    desired_state_vars: vec!["value"],
+                });
+            
+                HashMap::from([("children", instruction)]) 
+            },
+
+
+            determine_state_var_from_dependencies: |dependency_values| {
+                let value = dependency_values.dep_value("children")
+                    .has_exactly_one_element()
+                    .is_string();
+            
+                let num_val = value.parse::<f64>().unwrap_or(0.0);
+
+                SetValue(num_val)
+            },
+
+            ..Default::default()
+        }));
+
+        state_var_definitions.insert("hidden", HIDDEN_DEFAULT_DEFINITION());
+
+
+        return state_var_definitions
+    };
+}
+
+
+lazy_static! {
+    pub static ref MY_ATTRIBUTE_DEFINITIONS: HashMap<AttributeName, AttributeDefinition> = {
+        let mut attribute_definitions = HashMap::new();
+
+        attribute_definitions.insert("hide", AttributeDefinition::Component("boolean"));
+
+        attribute_definitions
+    };
+}
+
+
+impl ComponentSpecificBehavior for Number {
+
+    fn state_variable_instructions(&self) -> &'static HashMap<StateVarName, StateVarVariant> {
+        &MY_STATE_VAR_DEFINITIONS
+    }
+
+    fn attribute_instructions(&self) -> &'static HashMap<AttributeName, AttributeDefinition> {
+        &MY_ATTRIBUTE_DEFINITIONS
+    }
+
+    fn attributes(&self) -> &HashMap<AttributeName, Attribute> {
+        &self.attributes
+    }
+
+    fn should_render_children(&self) -> bool { false }
+
+    fn get_trait_names(&self) -> Vec<ObjectTraitName> {
+        vec![ObjectTraitName::NumberLike, ObjectTraitName::TextLike]
+    }
+
+    fn action_names(&self) -> Vec<&'static str> { vec![] }
+
+    fn get_copy_target_if_exists(&self) -> &Option<String> {
+        &self.copy_target
+    }
+}
+
+
+
+// impl TextLikeComponent for Number {
+//     fn text_value(&self) -> String {
+//         let val = *self.value.borrow();
+//         val.to_string()
+//     }
+// }
+// impl NumberLikeComponent for Number {
+//     fn add_one(&self) -> f64 {
+//         *self.value.borrow() + 1.0
+//     }
+// }
+
+
+
+impl Number {
+    pub fn create(name: String, parent: Option<String>, children: Vec<ComponentChild>, essential_state_vars: HashMap<StateVarName, EssentialStateVar>, attributes: HashMap<AttributeName, Attribute>, copy_target: Option<String>,
+
+    ) -> Box<dyn ComponentLike> {
+        Box::new(Number {
+            name,
+            parent,
+            children,
+
+            essential_state_vars,
+            attributes,
+            copy_target,
+            
+            value: StateVar::new(StateVarValueType::Number),
+            hidden: StateVar::new(StateVarValueType::Boolean),
+        })
+    }
+}
