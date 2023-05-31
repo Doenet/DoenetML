8d1b3a9cbf46f81f9267121ed9ea5830fb6e529b
Duane Nykamp <nykamp@umn.edu>
2023-05-31 16:06:08 -0500

    add more exports

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+export { default } from "./src/DoenetML.jsx";
 
-export {default} from "./src/DoenetML.jsx";
-
-export { default as ActivityViewer } from "./src/Viewer/ActivityViewer.jsx";
-export { default as PageViewer } from "./src/Viewer/PageViewer.jsx";
-export { mathjaxConfig} from "./src/Core/utils/math.js";
+export {
+  default as ActivityViewer,
+  activityAttemptNumberSetUpAtom,
+  currentPageAtom,
+  itemWeightsAtom,
+  saveStateToDBTimerIdAtom,
+} from "./src/Viewer/ActivityViewer";
+export {
+  default as PageViewer,
+  scrollableContainerAtom,
+} from "./src/Viewer/PageViewer";
+export { mathjaxConfig } from "./src/Core/utils/math";
+export { cidFromText } from "./src/Core/utils/cid";
+export { retrieveTextFileForCid } from "./src/Core/utils/retrieveTextFile";
+export {
+  calculateOrderAndVariants,
+  determineNumberOfActivityVariants,
+  parseActivityDefinition,
+  returnNumberOfActivityVariantsForCid,
+} from "./src/utils/activityUtils";
+export {
+  serializedComponentsReplacer,
+  serializedComponentsReviver,
+} from "./src/Core/utils/serializedStateProcessing";
+export { returnAllPossibleVariants } from "./src/Core/utils/returnAllPossibleVariants";
+export { default as CodeMirror } from "./src/Tools/CodeMirror";
+export {
+  default as DarkmodeController,
+  darkModeAtom,
+} from "./src/Tools/DarkmodeController";
