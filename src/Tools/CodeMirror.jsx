e24ebd7c27799e187ac237ff667ad1fc16a39864
Duane Nykamp <nykamp@umn.edu>
2023-06-03 04:38:50 +0000

    don't use recoil for CodeMirror

--- src/Tools/CodeMirror.jsx
+++ src/Tools/CodeMirror.jsx
@@ -21,15 +21,6 @@ import {
 import { HighlightStyle } from "@codemirror/highlight";
 import { completeFromSchema } from "@codemirror/lang-xml";
 import { parser } from "../Parser/doenet";
-import { atom, useRecoilValue } from "recoil";
-import { getRenderer } from "handsontable/renderers";
-
-const editorConfigStateAtom = atom({
-  key: "editorConfigStateAtom",
-  default: {
-    matchTag: false,
-  },
-});
 
 export default function CodeMirror({
   setInternalValueTo,
@@ -80,7 +71,9 @@ export default function CodeMirror({
     },
   });
 
-  let editorConfig = useRecoilValue(editorConfigStateAtom);
+  let [editorConfig, setEditorConfig] = useState({
+    matchTag: false,
+  });
   let view = useRef(null);
   let parent = useRef(null);
   const [count, setCount] = useState(0);
