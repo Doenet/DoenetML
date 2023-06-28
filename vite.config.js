ab2bd2d10a64be852bd369edaf2c9c2c3b661f40
Emilio Alvarez <e.l.alvarez@icloud.com>
2023-06-28 14:34:27 -0500

    cleanup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -25,42 +24,4 @@ export default defineConfig({
       NodeModulesPolyfillPlugin(),
     ],
   },
-  build: {
-    emptyOutDir: false,
-    minify: true,
-    lib: {
-      entry: "index.js",
-      name: "DoenetML",
-      fileName: "doenetml",
-      formats: ["es", "umd"],
-    },
-    rollupOptions: {
-      plugins: [nodePolyfills()],
-      external: ["react", "react-dom", "styled-components"],
-      output: {
-        globals: {
-          react: "react",
-          "react-dom": "react-dom",
-          "styled-components": "styled-components",
-        },
-      },
-    },
-  },
-  worker: {
-    format: "iife",
-  },
-  // commonjsOptions: {
-  //   transformMixedEsModules: true,
-  //   // Bugfix required to handle issue with vite, rollup and libs (like react-datetime)
-  //   // https://github.com/vitejs/vite/issues/2139#issuecomment-1399098579
-  //   defaultIsModuleExports(id) {
-  //     try {
-  //       const module = require(id);
-  //       if (module?.default) return false;
-  //       return "auto";
-  //     } catch (error) {
-  //       return "auto";
-  //     }
-  //   },
-  // },
 });
