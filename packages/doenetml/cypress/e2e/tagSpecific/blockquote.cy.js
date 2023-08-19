15034fa7fad334981626490a19826966054d3017
Duane Nykamp <nykamp@umn.edu>
2023-08-19 17:56:06 -0500

    Bug fixes (#9)
    * add git blame ignore
    * tweak tests, bug fixes
    * temporarily remove plotly

--- packages/doenetml/cypress/e2e/tagSpecific/blockquote.cy.js
+++ packages/doenetml/cypress/e2e/tagSpecific/blockquote.cy.js
@@ -3,7 +3,7 @@ import { cesc } from "../../../src/utils/url";
 describe("blockquote Tag Tests", function () {
     beforeEach(() => {
         cy.clearIndexedDB();
-        cy.visit("/src/Tools/cypressTest");
+        cy.visit("/src/Tools/cypressTest/");
     });
 
     it("display blockquote", () => {
