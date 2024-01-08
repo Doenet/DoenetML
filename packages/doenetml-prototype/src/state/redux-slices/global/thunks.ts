7182753cd4ab4d5ac11bea23b4ed58e062155a69
Jason Siefken <siefkenj@gmail.com>
2024-01-07 23:54:13 -0500

    Update styles and add Point/Line (#85)

--- packages/doenetml-prototype/src/state/redux-slices/global/thunks.ts
+++ packages/doenetml-prototype/src/state/redux-slices/global/thunks.ts
@@ -1,3 +1,29 @@
+import { globalActions } from ".";
+import { createLoggingAsyncThunk } from "../../hooks";
 import { _globalReducerActions } from "./slice";
 
-export const globalThunks = {};
+export const globalThunks = {
+    watchForDarkModePreferenceChange: createLoggingAsyncThunk(
+        "global/watchForDarkModePreferenceChange",
+        async (_: void, { dispatch }) => {
+            if (typeof window !== "undefined" && window.matchMedia) {
+                const mediaQuery = window.matchMedia(
+                    "(prefers-color-scheme: dark)",
+                );
+                const listener = (e: MediaQueryListEvent) => {
+                    dispatch(globalActions.setDarkMode(mediaQuery.matches));
+                };
+                mediaQuery.addEventListener("change", listener);
+                dispatch(globalActions.setDarkMode(mediaQuery.matches));
+            }
+        },
+    ),
+    setDarkMode: createLoggingAsyncThunk(
+        "global/setDarkMode",
+        async (darkMode: boolean, { dispatch }) => {
+            console.log("setDarkMode", darkMode);
+            document.querySelector("html")?.classList.toggle("dark", darkMode);
+            dispatch(_globalReducerActions._setDarkMode(darkMode));
+        },
+    ),
+};
--- packages/doenetml-prototype/tailwind.config.js
+++ packages/doenetml-prototype/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    mode: "jit",
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./src/**/*.css"],
     theme: {
         extend: {},
     },
