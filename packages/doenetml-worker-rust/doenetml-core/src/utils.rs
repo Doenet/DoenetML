b75eabab046cc735d5b9997302013ab8280cd09a
Duane Nykamp <nykamp@umn.edu>
2024-01-09 17:33:23 -0600

    tweak case-insensitive comparison

--- packages/doenetml-worker-rust/doenetml-core/src/utils.rs
+++ packages/doenetml-worker-rust/doenetml-core/src/utils.rs
@@ -11,10 +11,8 @@ where
     /// Match key to the HashMap keys, ignoring case.
     /// Return the original key-value pair the HashMap.
     fn get_key_value_ignore_case<'a>(&'a self, key: &str) -> Option<(&'a K, &'a V)> {
-        let lower_case_key = key.to_lowercase();
-
         for k in self.keys() {
-            if k.to_string().to_lowercase() == lower_case_key {
+            if k.to_string().eq_ignore_ascii_case(key) {
                 return Some((k, self.get(k).unwrap()));
             }
         }
