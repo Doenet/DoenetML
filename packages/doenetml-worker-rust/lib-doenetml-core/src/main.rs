695a9f3757d083615236ab5d7d543e38623ea14a
Jason Siefken <siefkenj@gmail.com>
2024-01-16 17:46:33 -0500

    Keep static version working

--- packages/doenetml-worker-rust/lib-doenetml-core/src/main.rs
+++ packages/doenetml-worker-rust/lib-doenetml-core/src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
-use doenetml_core::DoenetMLCore;
+use doenetml_core::{dast::DastRoot, DoenetMLCore};
 
 fn main() -> Result<()> {
     println!("Running DoenetML Core in Standalone Mode");
 
     let program = r#"{"type":"root","children":[{"type":"element","name":"document","attributes":{},"children":[{"type":"element","name":"text","attributes":{},"children":[{"type":"text","value":"hello "},{"type":"element","name":"text","attributes":{},"children":[{"type":"text","value":"there"}]}]}]}]}"#;
+    let dast_root: DastRoot = serde_json::from_str(program)?;
 
-    let mut core = DoenetMLCore::new(program, "", "", None);
+    let mut core = DoenetMLCore::new(dast_root, "", "", None);
 
     let result = core.to_flat_dast();
 
