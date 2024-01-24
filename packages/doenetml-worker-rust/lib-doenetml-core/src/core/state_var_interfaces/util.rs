d45cccd7aa3f6ca9a1aabb9087447304e0fb23a0
Jason Siefken <siefkenj@gmail.com>
2024-01-24 10:17:12 -0500

    Cleanup some functions

--- packages/doenetml-worker-rust/lib-doenetml-core/src/core/state_var_interfaces/util.rs
+++ packages/doenetml-worker-rust/lib-doenetml-core/src/core/state_var_interfaces/util.rs
@@ -8,27 +8,27 @@ pub fn create_dependency_instruction_from_extend_source(
     parameters: &StateVarParameters,
     state_var_idx: StateVarIdx,
 ) -> Option<DependencyInstruction> {
-    if let Some(ExtendSource::StateVar(extend_state_var_description)) = extending {
-        for state_var_match in extend_state_var_description.state_variable_matching.iter() {
-            if state_var_match
-                .shadowing_idx
-                .map(|sv_idx| sv_idx == state_var_idx)
-                .unwrap_or(parameters.is_primary_state_variable_for_shadowing_extend_source)
-            {
-                // Either
-                // 1. shadowing index was supplied and it matches the index of the state variable, or
-                // 2. shadowing index was not supplied and this variable is the primary state variable
-                //    for use when shadowing extend sources.
-                // Therefore, we shadow the extend source.
-
-                return Some(DependencyInstruction::StateVar {
-                    component_idx: Some(extend_state_var_description.component_idx),
-                    state_var_idx: state_var_match.shadowed_idx,
-                });
-            }
-        }
-    }
-    None
+    extending.and_then(|extend_source| match extend_source {
+        ExtendSource::StateVar(description) => description
+            .state_variable_matching
+            .iter()
+            .find(|state_var_match| {
+                state_var_match
+                    .shadowing_idx
+                    .map(|sv_idx| sv_idx == state_var_idx)
+                    .unwrap_or(parameters.is_primary_state_variable_for_shadowing_extend_source)
+            })
+            // Either
+            // 1. shadowing index was supplied and it matches the index of the state variable, or
+            // 2. shadowing index was not supplied and this variable is the primary state variable
+            //    for use when shadowing extend sources.
+            // Therefore, we shadow the extend source.
+            .map(|var| DependencyInstruction::StateVar {
+                component_idx: Some(description.component_idx),
+                state_var_idx: var.shadowed_idx,
+            }),
+        _ => None,
+    })
 }
 
 /// Convert string to boolean
