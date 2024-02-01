a95ee0c971515afbc32b008ad8a49a9e484e9545
Jason Siefken <siefkenj@gmail.com>
2024-01-31 23:20:04 -0500

    Make from doenet source functions available in tests. (#110)

--- packages/doenetml-worker-rust/lib-doenetml-core/src/lib.rs
+++ packages/doenetml-worker-rust/lib-doenetml-core/src/lib.rs
@@ -4,3 +4,6 @@ pub mod core;
 pub mod utils;
 
 pub use crate::core::*;
+
+#[cfg(any(test, feature = "testing"))]
+pub mod test_utils;
--- packages/doenetml-worker-rust/lib-doenetml-core/tests/parse-dast.ts
+++ packages/doenetml-worker-rust/lib-doenetml-core/tests/parse-dast.ts
@@ -6,6 +6,7 @@ import {
     DastNodes,
     DastAttribute,
     DastMacroPathPart,
+    filterPositionInfo,
 } from "../../../parser/dist";
 import { normalizeDocumentDast } from "../../../doenetml-prototype/src/state/redux-slices/dast/utils/normalize-dast";
 import yargs from "yargs";
@@ -15,48 +16,6 @@ function toDast(source: string) {
 }
 
 type AnyNode = DastNodes | DastAttribute | DastMacroPathPart;
-
-/**
- * Remove any `position` data from the DAST tree.
- */
-function stripPositionData(node: AnyNode): Omit<AnyNode, "position"> {
-    delete node.position;
-    switch (node.type) {
-        case "root":
-        case "element":
-        case "attribute":
-            node.children.forEach((child) => stripPositionData(child));
-            break;
-        case "macro":
-            Object.values(node.attributes).forEach((value) =>
-                stripPositionData(value),
-            );
-            node.path.forEach((child) => stripPositionData(child));
-            break;
-        case "function":
-            if (node.input) {
-                node.input.forEach((child) =>
-                    child.forEach((x) => stripPositionData(x)),
-                );
-            }
-            node.path.forEach((child) => stripPositionData(child));
-            break;
-        case "error":
-        case "cdata":
-        case "comment":
-        case "doctype":
-        case "instruction":
-        case "text":
-            break;
-        case "pathPart":
-            node.index.forEach((child) => {
-                // @ts-ignore
-                delete child.position;
-            });
-    }
-
-    return node;
-}
 
 // Parse command-line arguments with yargs
 const argv = yargs(process.argv.slice(2))
@@ -75,7 +34,7 @@ const argv = yargs(process.argv.slice(2))
 // Do the actual processing
 const parsed = toDast(argv.input);
 if (argv.stripPosition) {
-    stripPositionData(parsed);
+    filterPositionInfo(parsed);
 }
 // Call toDast with the input and write the result to stdout
 console.log(JSON.stringify(parsed));
