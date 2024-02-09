a52892739d9fb47248ecb83c1a315d84c3c894bb
Duane Nykamp <nykamp@umn.edu>
2024-02-09 13:36:07 -0600

    text input actions with references

--- packages/doenetml-worker-rust/lib-doenetml-core/src/core/components/doenet/text_input/test.rs
+++ packages/doenetml-worker-rust/lib-doenetml-core/src/core/components/doenet/text_input/test.rs
@@ -1,8 +1,11 @@
 use crate::{
     components::{
         actions::{Action, ActionBody},
-        doenet::text_input::{
-            test::test_helpers::*, TextInputAction, TextInputActionArgs, TextInputState,
+        doenet::{
+            text::TextState,
+            text_input::{
+                test::test_helpers::*, TextInputAction, TextInputActionArgs, TextInputState,
+            },
         },
         prelude::StateVarIdx,
         ActionsEnum,
@@ -11,9 +14,10 @@ use crate::{
     ComponentIdx, DoenetMLCore,
 };
 
-const IMMEDIATE_VALUE_IDX: ComponentIdx =
-    TextInputState::get_immediate_value_state_variable_index();
+const IMMEDIATE_VALUE_IDX: StateVarIdx = TextInputState::get_immediate_value_state_variable_index();
 const VALUE_IDX: StateVarIdx = TextInputState::get_value_state_variable_index();
+
+const TEXT_VALUE_IDX: StateVarIdx = TextState::get_value_state_variable_index();
 
 /// The `UpdateImmediateValue` action updates `immediate_value` but not `value`.
 ///
@@ -225,17 +229,129 @@ fn value_and_immediate_value_with_two_grandchildren_respond_to_actions() {
     assert_eq!(get_value(text_input_idx, &mut core), "hello there");
 }
 
+/// Children of text input will be used rather than prefill
+#[test]
+fn text_input_child_supersedes_prefill() {
+    let dast_root = dast_root_no_position(r#"<textInput prefill="ignored">hello</textInput>"#);
+    let mut core = DoenetMLCore::new(dast_root, "", "", None);
+
+    // the text input will be index 1, as the document tag will be index 0.
+    let text_input_idx = 1;
+
+    // both variables are initialized to blank strings
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+
+    // The UpdateImmediateValue action (which should be in response to typing characters)
+    // should only update the immediate_value state variable
+    let type_word_action = update_immediate_value_action(String::from("bye"), text_input_idx);
+
+    let _ = core.dispatch_action(type_word_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+
+    // The UpdateValue action (which should corresponds to pressing enter or blurring)
+    // should update value to match immediate value
+    let press_enter_action = update_value_action(text_input_idx);
+
+    let _ = core.dispatch_action(press_enter_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "bye");
+}
+
+/// A text component referenced as a child of a text input
+/// will be changed with `UpdateValue`
+#[test]
+fn text_input_reference_child_is_changed_when_update_value() {
+    let dast_root =
+        dast_root_no_position(r#"<textInput>$t</textInput><text name="t">hello</text>"#);
+    let mut core = DoenetMLCore::new(dast_root, "", "", None);
+
+    // the text input will be index 1, as the document tag will be index 0.
+    let text_input_idx = 1;
+
+    // the original text will be right after the text input, so index 2
+    let text_idx = 2;
+
+    // both variables are initialized to blank strings
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_text_value(text_idx, &mut core), "hello");
+
+    // The UpdateImmediateValue action (which should be in response to typing characters)
+    // should only update the immediate_value state variable
+    let type_word_action = update_immediate_value_action(String::from("bye"), text_input_idx);
+
+    let _ = core.dispatch_action(type_word_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_text_value(text_idx, &mut core), "hello");
+
+    // The UpdateValue action (which should corresponds to pressing enter or blurring)
+    // should update value to match immediate value
+    let press_enter_action = update_value_action(text_input_idx);
+
+    let _ = core.dispatch_action(press_enter_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_text_value(text_idx, &mut core), "bye");
+}
+
+/// A text component referenced as a prefill a text input
+/// will not be changed with `UpdateValue`
+#[test]
+fn text_input_reference_prefill_is_not_changed_when_update_value() {
+    let dast_root =
+        dast_root_no_position(r#"<textInput prefill="$t" /><text name="t">hello</text>"#);
+    let mut core = DoenetMLCore::new(dast_root, "", "", None);
+
+    // the text input will be index 1, as the document tag will be index 0.
+    let text_input_idx = 1;
+
+    // the original text will be right after the text input, so index 2
+    let text_idx = 2;
+
+    // both variables are initialized to blank strings
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_text_value(text_idx, &mut core), "hello");
+
+    // The UpdateImmediateValue action (which should be in response to typing characters)
+    // should only update the immediate_value state variable
+    let type_word_action = update_immediate_value_action(String::from("bye"), text_input_idx);
+
+    let _ = core.dispatch_action(type_word_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "hello");
+    assert_eq!(get_text_value(text_idx, &mut core), "hello");
+
+    // The UpdateValue action (which should corresponds to pressing enter or blurring)
+    // should update value to match immediate value
+    let press_enter_action = update_value_action(text_input_idx);
+
+    let _ = core.dispatch_action(press_enter_action);
+
+    assert_eq!(get_immediate_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_value(text_input_idx, &mut core), "bye");
+    assert_eq!(get_text_value(text_idx, &mut core), "hello");
+}
+
 mod test_helpers {
     use super::*;
 
-    /// Resolves `immediate_value` and returns its value as a `String`
+    /// Resolves `immediate_value` of a text input and returns its value as a `String`
     pub fn get_immediate_value(component_idx: ComponentIdx, core: &mut DoenetMLCore) -> String {
         core.get_state_var_value(component_idx, IMMEDIATE_VALUE_IDX)
             .try_into()
             .unwrap()
     }
 
-    /// Resolves `value` and returns its value as a `String`
+    /// Resolves `value` of a text input and returns its value as a `String`
     pub fn get_value(component_idx: ComponentIdx, core: &mut DoenetMLCore) -> String {
         core.get_state_var_value(component_idx, VALUE_IDX)
             .try_into()
@@ -258,5 +374,12 @@ mod test_helpers {
             component_idx,
             action: ActionsEnum::TextInput(TextInputAction::UpdateValue),
         }
+    }
+
+    /// Resolves `value` from a `<text>` component and returns its value as a `String`
+    pub fn get_text_value(component_idx: ComponentIdx, core: &mut DoenetMLCore) -> String {
+        core.get_state_var_value(component_idx, TEXT_VALUE_IDX)
+            .try_into()
+            .unwrap()
     }
 }
