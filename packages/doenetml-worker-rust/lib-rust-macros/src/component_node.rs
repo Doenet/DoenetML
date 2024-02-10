5709f329b6289c8502719f50dc245374ff6ced6d
Jason Siefken <siefkenj@gmail.com>
2024-02-10 15:30:44 -0500

    Formatting

--- packages/doenetml-worker-rust/lib-rust-macros/src/component_node.rs
+++ packages/doenetml-worker-rust/lib-rust-macros/src/component_node.rs
@@ -37,88 +37,88 @@ pub fn component_node_derive(input: TokenStream) -> TokenStream {
                 };
 
                 let mut component_node_impl_body = quote! {
-                        fn get_idx(&self) -> ComponentIdx {
-                            self.common.idx
-                        }
-                        fn get_parent(&self) -> Option<ComponentIdx> {
-                            self.common.parent
-                        }
-                        fn get_children(&self) -> &Vec<ComponentPointerTextOrMacro> {
-                            &self.common.children
-                        }
-                        fn set_children(&mut self, children: Vec<ComponentPointerTextOrMacro>) {
-                            self.common.children = children;
-                        }
-                        fn replace_children(&mut self, new_children: Vec<ComponentPointerTextOrMacro>) -> Vec<ComponentPointerTextOrMacro> {
-                            std::mem::replace(&mut self.common.children, new_children)
-                        }
+                    fn get_idx(&self) -> ComponentIdx {
+                        self.common.idx
+                    }
+                    fn get_parent(&self) -> Option<ComponentIdx> {
+                        self.common.parent
+                    }
+                    fn get_children(&self) -> &Vec<ComponentPointerTextOrMacro> {
+                        &self.common.children
+                    }
+                    fn set_children(&mut self, children: Vec<ComponentPointerTextOrMacro>) {
+                        self.common.children = children;
+                    }
+                    fn replace_children(&mut self, new_children: Vec<ComponentPointerTextOrMacro>) -> Vec<ComponentPointerTextOrMacro> {
+                        std::mem::replace(&mut self.common.children, new_children)
+                    }
 
-                        fn initialize(
-                            &mut self,
-                            idx: ComponentIdx,
-                            parent: Option<ComponentIdx>,
-                            extending: Option<ExtendSource>,
-                            attributes: HashMap<String, DastAttribute>,
-                            position: Option<DastPosition>,
-                        ) {
-                            self.common.idx = idx;
-                            self.common.parent = parent;
-                            self.common.extending = extending;
-                            self.common.position = position;
-                            self.common.unevaluated_attributes = attributes;
-                        }
+                    fn initialize(
+                        &mut self,
+                        idx: ComponentIdx,
+                        parent: Option<ComponentIdx>,
+                        extending: Option<ExtendSource>,
+                        attributes: HashMap<String, DastAttribute>,
+                        position: Option<DastPosition>,
+                    ) {
+                        self.common.idx = idx;
+                        self.common.parent = parent;
+                        self.common.extending = extending;
+                        self.common.position = position;
+                        self.common.unevaluated_attributes = attributes;
+                    }
 
-                        fn get_extending(&self) -> Option<&ExtendSource> {
-                            self.common.extending.as_ref()
-                        }
+                    fn get_extending(&self) -> Option<&ExtendSource> {
+                        self.common.extending.as_ref()
+                    }
 
-                        fn get_component_type(&self) -> &str {
-                            #component_string
-                        }
-                        fn get_descendant_matches(&self, name: &str) -> Option<&Vec<ComponentIdx>> {
-                            self.common.descendant_names.get(name)
-                        }
-                        fn set_descendant_names(&mut self, descendant_names: HashMap<String, Vec<ComponentIdx>>) {
-                            self.common.descendant_names = descendant_names;
-                        }
+                    fn get_component_type(&self) -> &str {
+                        #component_string
+                    }
+                    fn get_descendant_matches(&self, name: &str) -> Option<&Vec<ComponentIdx>> {
+                        self.common.descendant_names.get(name)
+                    }
+                    fn set_descendant_names(&mut self, descendant_names: HashMap<String, Vec<ComponentIdx>>) {
+                        self.common.descendant_names = descendant_names;
+                    }
 
-                        fn get_position(&self) -> Option<&DastPosition> {
-                            self.common.position.as_ref()
-                        }
+                    fn get_position(&self) -> Option<&DastPosition> {
+                        self.common.position.as_ref()
+                    }
 
-                        fn set_position(&mut self, position: Option<DastPosition>) {
-                            self.common.position = position;
-                        }
+                    fn set_position(&mut self, position: Option<DastPosition>) {
+                        self.common.position = position;
+                    }
 
-                        fn set_attribute_children(
-                            &mut self,
-                            attribute_children: HashMap<AttributeName, Vec<ComponentPointerTextOrMacro>>,
-                        ) {
-                            self.common.attribute_children = attribute_children;
-                        }
+                    fn set_attribute_children(
+                        &mut self,
+                        attribute_children: HashMap<AttributeName, Vec<ComponentPointerTextOrMacro>>,
+                    ) {
+                        self.common.attribute_children = attribute_children;
+                    }
 
-                        fn get_attribute_children_for_attribute(
-                            &self,
-                            attribute: AttributeName,
-                        ) -> Option<&Vec<ComponentPointerTextOrMacro>> {
-                            self.common.attribute_children.get(attribute)
-                        }
+                    fn get_attribute_children_for_attribute(
+                        &self,
+                        attribute: AttributeName,
+                    ) -> Option<&Vec<ComponentPointerTextOrMacro>> {
+                        self.common.attribute_children.get(attribute)
+                    }
 
-                        fn get_unevaluated_attributes(&self) -> &HashMap<String, DastAttribute> {
-                            &self.common.unevaluated_attributes
-                        }
+                    fn get_unevaluated_attributes(&self) -> &HashMap<String, DastAttribute> {
+                        &self.common.unevaluated_attributes
+                    }
 
-                        fn get_unevaluated_attributes_mut(&mut self) -> &mut HashMap<String, DastAttribute> {
-                            &mut self.common.unevaluated_attributes
-                        }
+                    fn get_unevaluated_attributes_mut(&mut self) -> &mut HashMap<String, DastAttribute> {
+                        &mut self.common.unevaluated_attributes
+                    }
 
-                        fn get_is_in_render_tree(&self) -> bool {
-                            self.common.is_in_render_tree
-                        }
+                    fn get_is_in_render_tree(&self) -> bool {
+                        self.common.is_in_render_tree
+                    }
 
-                        fn set_is_in_render_tree(&mut self, is_in_render_tree: bool) {
-                            self.common.is_in_render_tree = is_in_render_tree;
-                        }
+                    fn set_is_in_render_tree(&mut self, is_in_render_tree: bool) {
+                        self.common.is_in_render_tree = is_in_render_tree;
+                    }
                 };
                 if let Some(ref_transmute_to) = &options.ref_transmutes_to {
                     component_node_impl_body.extend(quote! {
