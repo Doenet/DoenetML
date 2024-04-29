4a79bbcae991e4076c01e74374c1c6b3a07785d1
Virginia Mae <vmchristian@gmail.com>
2024-04-29 13:44:51 -0500

    this subset is broken

--- packages/docs-nextra/next.config.mjs
+++ packages/docs-nextra/next.config.mjs
@@ -1,8 +1,8 @@
-import React from "react";
 import nextraConfig from "nextra";
 const withNextra = nextraConfig({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
+    defaultShowCopyCode: true,
 });
 
 export default withNextra();
--- packages/docs-nextra/theme.config.tsx
+++ packages/docs-nextra/theme.config.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>DoenetML Documentation</span>,
     project: {
         link: "https://github.com/shuding/nextra",
     },
-    head: (
-        <>
-          <script type="module" src="https://dev.doenet.org/cdn/doenet-standalone.js"> </script>
-          <link rel="stylesheet" href="https://dev.doenet.org/cdn/style.css" />
-        </>
-    )
     // ... other theme options
 };
