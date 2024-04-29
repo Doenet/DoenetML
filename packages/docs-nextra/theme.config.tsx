e65ee2051224f1be5620e323584c9ded4b3094b6
Virginia Mae <vmchristian@gmail.com>
2024-04-29 14:08:25 -0500

    Add back one important change from branch with jason's fixes to make the build faster

--- packages/docs-nextra/theme.config.tsx
+++ packages/docs-nextra/theme.config.tsx
@@ -5,5 +5,11 @@ export default {
     project: {
         link: "https://github.com/shuding/nextra",
     },
+    head: (
+        <>
+            <script type="module" src="https://dev.doenet.org/cdn/doenet-standalone.js"> </script>
+            <link rel="stylesheet" href="https://dev.doenet.org/cdn/style.css" />
+        </>
+    )
     // ... other theme options
 };
