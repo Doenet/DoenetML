50e1a3af68f0a3ed87c65d1d6cfd1abf91d6d4fa
Jason Altekruse <altekrusejason@gmail.com>
2024-04-29 11:20:38 -0500

    Move head configuration to the proper place in the theme config

--- packages/docs-nextra/next.config.mjs
+++ packages/docs-nextra/next.config.mjs
@@ -3,12 +3,6 @@ import nextraConfig from "nextra";
 const withNextra = nextraConfig({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
-    head: (
-        <>
-          <script type="module" src="https://dev.doenet.org/cdn/doenet-standalone.js"> </script>
-          <link rel="stylesheet" href="https://dev.doenet.org/cdn/style.css" />
-        </>
-    )
 });
 
 export default withNextra();
