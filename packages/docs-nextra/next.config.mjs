ed021d712428d6127627424a315214b8edcc038c
Virginia Mae <vmchristian@gmail.com>
2024-05-10 10:27:16 -0500

    Docs - expanded tutorials 1-6

--- packages/docs-nextra/next.config.mjs
+++ packages/docs-nextra/next.config.mjs
@@ -3,7 +3,13 @@ const withNextra = nextraConfig({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
     defaultShowCopyCode: true,
+    latex: true,
 });
+
+
+// module.exports = require('nextra')({
+//     latex: true
+//   }); 
 
 export default withNextra();
 
