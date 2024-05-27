72ba9c0735660be9169d2e46c27a14ab03a35d05
Duane Nykamp <nykamp@umn.edu>
2024-05-26 20:15:37 -0500

    skeleton of a test

---
+++ packages/test-cypress/cypress/e2e/tagSpecific/regionBetweenCurves.cy.js
@@ -0,0 +1,38 @@
+import { cesc2 } from "@doenet/utils";
+
+describe("RegionBetweenCurves Tag Tests", function () {
+    beforeEach(() => {
+        cy.clearIndexedDB();
+        cy.visit("/");
+    });
+
+    it("region between two curves", () => {
+        cy.window().then(async (win) => {
+            win.postMessage(
+                {
+                    doenetML: `
+  <text>a</text>
+  <graph name="g1" newNamespace>
+    <function name="f1">sin(x)</function>
+    <function name="f2">cos(x)</function>
+    <regionBetweenCurves name="r" boundaryValues="-3 5">$f1 $f2</regionBetweenCurves>
+  </graph>
+
+  <graph name="g2" newNamespace>
+    $(../g1/r{name="r"})
+  </graph>
+
+  $g2{name="g3"}
+
+
+  `,
+                },
+                "*",
+            );
+        });
+
+        cy.get(cesc2("#/_text1")).should("have.text", "a"); // to wait for page to load
+
+        // Not sure what to test until can test jsxgraph output
+    });
+});
