10c115fe79f44030376af063e33c9c59bf92afeb
Virginia Mae <vmchristian@gmail.com>
2024-05-30 09:29:52 -0500

    added tutorials 2.1 - 2.4; trying to fix theme.config to show folders in sidebar closed as default

--- packages/docs-nextra/theme.config.tsx
+++ packages/docs-nextra/theme.config.tsx
@@ -10,6 +10,10 @@ export default {
             <script type="module" src="https://dev.doenet.org/cdn/doenet-standalone.js"> </script>
             <link rel="stylesheet" href="https://dev.doenet.org/cdn/style.css" />
         </>
-    )
+    ),
+    sidebar: {
+        "defaultMenuCollapseLevel": "1",
+        "auto collapse": true,
+    },
     // ... other theme options
 };
