dff41ce834634a07cd215283256f51805b86d69e
Duane Nykamp <nykamp@umn.edu>
2024-06-10 11:53:53 -0500

    tweak packaging and fix font issue

--- packages/standalone/vite.config.ts
+++ packages/standalone/vite.config.ts
@@ -5,6 +5,9 @@ import * as path from "node:path";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+// These are the dependencies that will not be bundled into the library.
+const EXTERNAL_DEPS = [];
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
@@ -18,6 +21,11 @@ export default defineConfig({
                         "../fonts/*",
                     ),
                     dest: "fonts/",
+                },
+                {
+                    src: "package.json",
+                    dest: "./",
+                    transform: transformPackageJson,
                 },
             ],
         }),
@@ -42,3 +50,72 @@ export default defineConfig({
         "process.env.NODE_ENV": '"production"',
     },
 });
+
+/**
+ * Trim and modify the `package.json` file so that it is suitable for publishing.
+ */
+function transformPackageJson(contents: string, filePath: string) {
+    const pkg = JSON.parse(contents);
+    const allDeps = {
+        ...pkg.dependencies,
+        ...pkg.peerDependencies,
+        ...pkg.devDependencies,
+    };
+    // Delete unneeded entries
+    delete pkg.private;
+    delete pkg.scripts;
+    delete pkg.devDependencies;
+    delete pkg.peerDependencies;
+    delete pkg.dependencies;
+    delete pkg.prettier;
+    delete pkg.wireit;
+
+    pkg.private = false;
+
+    // Everything that is externalized should be a peer dependency
+    pkg.peerDependencies = {};
+    for (const dep of EXTERNAL_DEPS) {
+        if (!allDeps[dep]) {
+            console.warn(
+                dep,
+                "is listed as a dependency for vite to externalize, but a version is not specified in package.json.",
+            );
+            continue;
+        }
+        pkg.peerDependencies[dep] = allDeps[dep];
+    }
+
+    // Fix up the paths. The existing package.json refers to files in the `./dist` directory. But
+    // the new package.json will be in the ./dist directory itself, so we need to remove any `./dist`
+    // prefix from the paths.
+    const outputPackageJsonPath = path.join(
+        path.dirname(filePath),
+        "./dist/package.json",
+    );
+    if (Array.isArray(pkg.files)) {
+        pkg.files = pkg.files.map((file) =>
+            getPathRelativeToPackageJson(file, outputPackageJsonPath),
+        );
+    }
+    for (const exp of Object.values(pkg.exports ?? {}) as Record<
+        string,
+        string
+    >[]) {
+        for (const [format, path] of Object.entries(exp)) {
+            exp[format] = getPathRelativeToPackageJson(
+                path,
+                outputPackageJsonPath,
+            );
+        }
+    }
+
+    return JSON.stringify(pkg, null, 4);
+}
+
+function getPathRelativeToPackageJson(
+    relPath: string,
+    packageJsonPath: string,
+) {
+    const packageJsonDir = path.dirname(packageJsonPath);
+    return "./" + path.relative(packageJsonDir, path.join(__dirname, relPath));
+}
