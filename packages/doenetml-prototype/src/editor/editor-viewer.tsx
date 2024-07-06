82f6c96a152ba728e7f6bfbe995922b2c865e5d1
Jason Siefken <siefkenj@gmail.com>
2024-07-06 14:11:57 -0400

    Add dep

--- packages/doenetml-prototype/src/editor/editor-viewer.tsx
+++ packages/doenetml-prototype/src/editor/editor-viewer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CodeMirror } from "@doenet/codemirror";
 import { ResizablePanelPair } from "./components/resizable-panel-pair";
 import { DoenetML } from "../DoenetML";
+import { Button } from "@ariakit/react";
 
 export type EditorViewerProps = {
     doenetML: string;
@@ -11,10 +12,40 @@ export type EditorViewerProps = {
  * A component that renders A source editor and rendered doenetml side-by-side.
  */
 export function EditorViewer({ doenetML = "" }: EditorViewerProps) {
+    const [sourceForRender, setSourceForRender] = React.useState(doenetML);
+    const [sourceInEditor, setSourceInEditor] = React.useState(doenetML);
+
+    const canRefresh = sourceInEditor !== sourceForRender;
+
     return (
-        <ResizablePanelPair
-            panelA={<CodeMirror value={doenetML} />}
-            panelB={<DoenetML doenetML={doenetML} />}
-        />
+        <div className="editor-viewer">
+            <div className="editor-viewer-header">
+                <Button
+                    disabled={!canRefresh}
+                    title={
+                        canRefresh
+                            ? "Refresh the rendered code"
+                            : "The code has not changes since the last render"
+                    }
+                >
+                    Refresh
+                </Button>
+            </div>
+            <div className="editor-viewer-panels">
+                <ResizablePanelPair
+                    panelA={
+                        <CodeMirror
+                            value={sourceInEditor}
+                            onChange={(source) => {
+                                if (source !== sourceInEditor) {
+                                    setSourceInEditor(source);
+                                }
+                            }}
+                        />
+                    }
+                    panelB={<DoenetML doenetML={sourceForRender} />}
+                />
+            </div>
+        </div>
     );
 }
