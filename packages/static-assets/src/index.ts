12e898eec192c2ac7fbeeb75c49e2a134ddc52e6
Jason Siefken <siefkenj@gmail.com>
2024-08-01 18:52:34 -0400

    Export relaxng schema as well as old schema

--- packages/static-assets/src/index.ts
+++ packages/static-assets/src/index.ts
@@ -1,4 +1,29 @@
 import doenetSchema from "./generated/doenet-schema.json";
+import _doenetRelaxngSchema from "./generated/doenet-relaxng-schema.json";
 import _ENTITY_MAP from "./generated/entity-map.json";
 const ENTITY_MAP: Record<string, string> = _ENTITY_MAP;
-export { doenetSchema, ENTITY_MAP };
+
+export type SchemaProperty = {
+    name: string;
+    type: string;
+    isArray: boolean;
+    numDimensions?: number;
+    indexedArrayDescription?: unknown[];
+};
+
+export type RelaxNgSchema = {
+    startType: string | string[];
+    refs: Record<string, RelaxNgAttribute>;
+};
+
+export type RelaxNgAttribute = {
+    type: "element";
+    name: string;
+    attributes: Record<string, { optional: boolean; type: string[] }>;
+    children: { ref: string }[];
+    properties?: SchemaProperty[];
+    textChildrenAllowed: boolean;
+};
+
+const doenetRelaxngSchema: RelaxNgSchema = _doenetRelaxngSchema as any;
+export { doenetSchema, ENTITY_MAP, doenetRelaxngSchema };
