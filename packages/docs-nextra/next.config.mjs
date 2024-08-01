23d5ce8ffedceef9edccf14e7e7417954c025851
Duane Nykamp <nykamp@umn.edu>
2024-07-31 23:01:52 -0500

    Nextra path does not include github repository name

--- packages/docs-nextra/next.config.mjs
+++ packages/docs-nextra/next.config.mjs
@@ -118,18 +118,9 @@ const withNextra = nextraConfig({
 //     latex: true
 //   });
 //
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = "";
 let basePath = "";
-
-if (isGithubActions) {
-    // trim off `<owner>/`
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
 
 export default withNextra({
     output: "export",
