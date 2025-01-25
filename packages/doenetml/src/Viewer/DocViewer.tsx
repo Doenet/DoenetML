b3c5aa6688ea9b8d49647ce9c9f85c4947aad6af
Duane Nykamp <nykamp@umn.edu>
2025-01-24 23:07:26 -0600

    add activityId and docId to all SPLICE calls (#282)
    * assume loaded state is already parsed
    * accept messages where window or parent location matches origin

--- packages/doenetml/src/Viewer/DocViewer.tsx
+++ packages/doenetml/src/Viewer/DocViewer.tsx
@@ -417,21 +417,29 @@ export function DocViewer({
                 window.postMessage({
                     ...e.data,
                     subject: "SPLICE.reportScoreAndState",
+                    activityId,
+                    docId,
                 });
             } else if (e.data.messageType === "recordSolutionView") {
                 window.postMessage({
                     ...e.data,
                     subject: "SPLICE.recordSolutionView",
+                    activityId,
+                    docId,
                 });
             } else if (e.data.messageType === "sendEvent") {
                 window.postMessage({
                     ...e.data,
                     subject: "SPLICE.sendEvent",
+                    activityId,
+                    docId,
                 });
             } else if (e.data.messageType === "allPossibleVariants") {
                 window.postMessage({
                     ...e.data,
                     subject: "SPLICE.allPossibleVariants",
+                    activityId,
+                    docId,
                 });
             } else if (e.data.messageType === "terminated") {
                 reinitializeCoreAndTerminateAnimations();
@@ -448,8 +456,11 @@ export function DocViewer({
     }, []);
 
     useEffect(() => {
-        window.addEventListener("message", (e) => {
-            if (e.origin !== window.location.origin) {
+        const listener = function (e: MessageEvent) {
+            if (
+                e.origin !== window.location.origin &&
+                e.origin !== window.parent.location.origin
+            ) {
                 return;
             }
             if (typeof e.data !== "object") {
@@ -468,8 +479,14 @@ export function DocViewer({
                     promiseInfo.reject(e.data);
                 }
             }
-        });
-    });
+        };
+
+        window.addEventListener("message", listener);
+
+        return () => {
+            window.removeEventListener("message", listener);
+        };
+    }, []);
 
     useEffect(() => {
         if (!coreWorker) {
@@ -1083,7 +1100,7 @@ export function DocViewer({
                         userId,
                     });
                     if (resp.loadedState) {
-                        processLoadedDocState(JSON.parse(resp.state));
+                        processLoadedDocState(resp.state);
                     }
                 } catch (e: any) {
                     setIsInErrorState?.(true);
