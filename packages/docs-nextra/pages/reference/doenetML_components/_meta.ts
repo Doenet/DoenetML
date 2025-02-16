45e9ce85e180a1d2c9cb048c521fd1fdd694a610
Jason Siefken <siefkenj@gmail.com>
2025-02-15 19:21:26 -0500

    Remove non-referenced elements

--- packages/docs-nextra/pages/reference/doenetML_components/_meta.ts
+++ packages/docs-nextra/pages/reference/doenetML_components/_meta.ts
@@ -22,12 +22,6 @@ export default {
     aside: {
         display: "hidden",
     },
-    asList: {
-        display: "hidden",
-    },
-    atom: {
-        display: "hidden",
-    },
     boolean: {
         display: "hidden",
     },
