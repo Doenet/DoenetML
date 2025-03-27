0372069a4035200b3782aeec1a520f04b3e11b55
Duane Nykamp <nykamp@umn.edu>
2025-03-26 19:35:38 -0500

    load tex-chtml-full mathjax component

--- packages/utils/src/math/math.ts
+++ packages/utils/src/math/math.ts
@@ -10,13 +10,5 @@ export const mathjaxConfig = {
             sech: "\\operatorname{sech}",
         },
         displayMath: [["\\[", "\\]"]],
-        packages: ["base", "ams", "noerrors", "noundefined", "configmacros"],
-    },
-    options: {
-        ignoreHtmlClass: "tex2jax_ignore",
-        processHtmlClass: "tex2jax_process",
-    },
-    loader: {
-        load: ["[tex]/noerrors"],
     },
 };
