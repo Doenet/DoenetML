19e6350745645dcfa03326d3d886f39d68a58294
Jason Siefken <siefkenj@gmail.com>
2025-06-12 13:59:21 -0400

    Add missing files

---
+++ packages/parser/src/detect-version.ts
@@ -0,0 +1,54 @@
+import { lezerToDast } from "./lezer-to-dast";
+import { DastElement } from "./types";
+
+/**
+ * If doenetML consists of a single `<document>` component with,
+ * for example, `xmlns="https://doenet.org/spec/doenetml/v0.7.0-alpha44"` set,
+ * attempt to extract the DoenetML version from that prop.
+ * Returns the version if found, else returns null.
+ */
+export function detectVersionFromDoenetML(
+    doenetML: string,
+): { version: string; position: { begin: number; end: number } } | null {
+    const parsed = lezerToDast(doenetML);
+    // Find the first document child and look for the xmlns attribute
+    const documentElement = parsed.children.find(
+        (e) => e.type === "element" && e.name === "document",
+    ) as DastElement;
+    if (!documentElement || !("xmlns" in documentElement.attributes)) {
+        return null;
+    }
+    const xmlnsValue = documentElement.attributes["xmlns"];
+    if (
+        xmlnsValue.type !== "attribute" ||
+        xmlnsValue.children.length !== 1 ||
+        xmlnsValue.children[0].type !== "text"
+    ) {
+        return null;
+    }
+    // Should now be a URL like https://doenet.org/spec/doenetml/v0.7.0-alpha44
+    const xmlnsTextNode = xmlnsValue.children[0];
+    const xmlnsText = xmlnsValue.children[0].value;
+
+    const match = xmlnsText.match(
+        /^https:\/\/doenet\.org\/spec\/doenetml\/v([0-9a-zA-Z\.\-]+)$/,
+    );
+
+    const documentTag = match ? match[0] : null;
+    const versionString = match ? match[1] : null;
+    if (match && documentTag && versionString != null) {
+        // Calculate the offset of the match
+        const offsetStart =
+            (match.index || 0) + documentTag.indexOf(versionString);
+        const offsetEnd = offsetStart + versionString.length;
+        return {
+            version: versionString,
+            position: {
+                begin: xmlnsTextNode.position?.start.offset! + offsetStart,
+                end: xmlnsTextNode.position?.start.offset! + offsetEnd + 1,
+            },
+        };
+    }
+
+    return null;
+}
---
+++ packages/parser/test/detect-version.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from "vitest";
+import { lezerToDast } from "../src/lezer-to-dast";
+import { toXml } from "../src/dast-to-xml/dast-util-to-xml";
+import util from "util";
+import { DastRoot } from "../src/types";
+import { detectVersionFromDoenetML } from "../src/detect-version";
+
+const origLog = console.log;
+console.log = (...args) => {
+    origLog(...args.map((x) => util.inspect(x, false, 10, true)));
+};
+
+describe("detect version from namespace", () => {
+    let source: string;
+    let version: ReturnType<typeof detectVersionFromDoenetML>;
+    let versionString: string;
+
+    it("can detect doenetml version from namespace on document tag", () => {
+        versionString = "0.7.0-alpha44";
+        source = `<document xmlns="https://doenet.org/spec/doenetml/v${versionString}"><p>content</p></document>`;
+        version = detectVersionFromDoenetML(source);
+
+        expect(version).toMatchObject({
+            version: versionString,
+        });
+        expect(
+            source.slice(version?.position.begin, version?.position.end),
+        ).toEqual("v" + versionString);
+
+        source = `<document \nother-attr="https://doenet.org/spec/doenetml/v10"  xmlns="https://doenet.org/spec/doenetml/v${versionString}"><p>content</p></document>`;
+        version = detectVersionFromDoenetML(source);
+
+        expect(version).toMatchObject({
+            version: versionString,
+        });
+        expect(
+            source.slice(version?.position.begin, version?.position.end),
+        ).toEqual("v" + versionString);
+    });
+    it("returns null on invalid or missing versions", () => {
+        versionString = "0.7.0-alpha44";
+        source = `<document xmlns="https://doenet.org/spec/doenetml/x${versionString}"><p>content</p></document>`;
+        version = detectVersionFromDoenetML(source);
+
+        expect(version).toEqual(null);
+
+        source = `<document xmlnsXXX="https://doenet.org/spec/doenetml/v${versionString}"><p>content</p></document>`;
+        version = detectVersionFromDoenetML(source);
+
+        expect(version).toEqual(null);
+
+        source = `<document><p>content</p></document>`;
+        version = detectVersionFromDoenetML(source);
+
+        expect(version).toEqual(null);
+    });
+});
