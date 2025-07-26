46a8c287ef3b7172a0883bacd6eca6c10503a6d3
Duane Nykamp <nykamp@umn.edu>
2025-07-26 11:34:36 -0500

    showCorrectness attribute on section and document (#566)

--- packages/doenetml-worker-javascript/src/components/Document.js
+++ packages/doenetml-worker-javascript/src/components/Document.js
@@ -41,6 +41,11 @@ export default class Document extends BaseComponent {
             defaultValue: false,
             public: true,
         };
+        attributes.showCorrectness = {
+            createComponentOfType: "boolean",
+            createStateVariable: "showCorrectnessPreliminary",
+            defaultValue: null,
+        };
         attributes.submitLabel = {
             createComponentOfType: "text",
             createStateVariable: "submitLabel",
@@ -478,14 +483,24 @@ export default class Document extends BaseComponent {
         stateVariableDefinitions.showCorrectness = {
             forRenderer: true,
             returnDependencies: () => ({
+                showCorrectnessPreliminary: {
+                    dependencyType: "stateVariable",
+                    variableName: "showCorrectnessPreliminary",
+                },
                 showCorrectnessFlag: {
                     dependencyType: "flag",
                     flagName: "showCorrectness",
                 },
             }),
-            definition({ dependencyValues }) {
-                let showCorrectness =
-                    dependencyValues.showCorrectnessFlag !== false;
+            definition({ dependencyValues, usedDefault }) {
+                let showCorrectness;
+                if (!usedDefault.showCorrectnessPreliminary) {
+                    showCorrectness =
+                        dependencyValues.showCorrectnessPreliminary;
+                } else {
+                    showCorrectness =
+                        dependencyValues.showCorrectnessFlag !== false;
+                }
                 return { setValue: { showCorrectness } };
             },
         };
