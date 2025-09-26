42e1e9b26e30dee114566838059c68eadc566c16
Duane Nykamp <nykamp@umn.edu>
2025-09-26 12:09:24 -0500

    convert tutorials to 0.7 (#609)

--- packages/docs-nextra/theme.config.tsx
+++ packages/docs-nextra/theme.config.tsx
@@ -5,20 +5,6 @@ export default {
     project: {
         link: "https://github.com/shuding/nextra",
     },
-    head: (
-        <>
-            <script
-                type="module"
-                src="https://dev.doenet.org/cdn/doenet-standalone.js"
-            >
-                {" "}
-            </script>
-            <link
-                rel="stylesheet"
-                href="https://dev.doenet.org/cdn/style.css"
-            />
-        </>
-    ),
     sidebar: {
         defaultMenuCollapseLevel: "1",
         "auto collapse": true,
