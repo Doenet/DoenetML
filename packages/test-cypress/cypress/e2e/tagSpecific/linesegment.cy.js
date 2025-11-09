74d9623fa1909586be0c1cb13d29b74e02268698
Duane Nykamp <nykamp@umn.edu>
2025-11-09 05:07:01 -0600

    line segment renderer does not crash with complex endpoints

---
+++ packages/test-cypress/cypress/e2e/tagSpecific/linesegment.cy.js
@@ -0,0 +1,29 @@
+import { cesc } from "@doenet/utils";
+
+describe("Line Tag Tests", function () {
+    beforeEach(() => {
+        cy.clearIndexedDB();
+        cy.visit("/");
+    });
+
+    it("complex endpoint doesn't crash renderer", () => {
+        cy.window().then(async (win) => {
+            win.postMessage(
+                {
+                    doenetML: `
+    <graph>
+        <lineSegment name="ls" endpoints="(1,0) (1,2i)"  />
+    </graph>
+    <p name="p">Endpoints: <point name="P1" extend="$ls.endpoint1" />, <point name="P2" extend="$ls.endpoint2" /></p>
+    `,
+                },
+                "*",
+            );
+        });
+
+        cy.get(cesc("#P1")).should("have.text", "(1,0)");
+        cy.get(cesc("#P2")).should("have.text", "(1,2i)");
+
+
+    });
+});
