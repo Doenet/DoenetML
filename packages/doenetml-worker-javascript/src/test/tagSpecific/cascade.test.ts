d2d614cc912691fd7bc2a6cc0a2d85f8b55f61bd
Duane Nykamp <nykamp@umn.edu>
2025-11-24 21:19:02 -0600

    continuation message tests

--- packages/doenetml-worker-javascript/src/test/tagSpecific/cascade.test.ts
+++ packages/doenetml-worker-javascript/src/test/tagSpecific/cascade.test.ts
@@ -1108,4 +1108,586 @@ describe("Cascade tag tests", async () => {
         await submitAnswer({ componentIdx: answer2Idx, core });
         await check_values(3);
     });
+
+    it("continuation messages inside sections", async () => {
+        let { core, resolvePathToNodeIdx } = await createTestCore({
+            doenetML: `
+<cascade name="w">
+  <title>My cascade</title>
+
+  <section boxed name="section1">
+    <title>First part</title>
+
+    <p name="p1">What is 1+1? <answer name="ans">2</answer></p>
+    <p name="p2">What is 1-1? <answer name="ans">0</answer></p>
+    <continuationMessage name="cm">Never shown</continuationMessage>
+  </section>
+
+
+  <section boxed name="section2">
+    <title>Second part</title>
+
+    <p name="p">What is 3+4? <answer name="ans">7</answer></p>
+    <continuationMessage name="cm"><em>Complete first part to proceed.</em></continuationMessage>
+  </section>
+
+  <section boxed name="section3">
+    <title>Third part</title>
+
+    <continuationMessage name="cm"><em>Complete second part to proceed.</em></continuationMessage>
+
+    <p name="p">What is 3-4? <answer name="ans">-1</answer></p>
+  </section>
+
+</cascade>
+
+<p name="pNumCompleted">numCompleted: $w.numCompleted</p>
+
+  `,
+        });
+
+        async function check_values(numCompleted: number) {
+            const stateVariables = await core.returnAllStateVariables(
+                false,
+                true,
+            );
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("w")].stateValues
+                    .title,
+            ).eq("My cascade");
+            expect(
+                stateVariables[await resolvePathToNodeIdx("w")].stateValues
+                    .numCompleted,
+            ).eq(numCompleted);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("pNumCompleted")]
+                    .stateValues.text,
+            ).eq(`numCompleted: ${numCompleted}`);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1")]
+                    .stateValues.hideChildren,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1")]
+                    .stateValues.titleColor,
+            ).eq(
+                stateVariables[await resolvePathToNodeIdx("section1")]
+                    .stateValues.creditAchieved < 1
+                    ? "var(--mainGray)"
+                    : "var(--lightGreen)",
+            );
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1.p1")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1.p2")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section1.cm")]
+                    .stateValues.hidden,
+            ).eq(true);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section2")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section2")]
+                    .stateValues.hideChildren,
+            ).eq(numCompleted < 1);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section2")]
+                    .stateValues.titleColor,
+            ).eq(
+                stateVariables[await resolvePathToNodeIdx("section2")]
+                    .stateValues.creditAchieved < 1
+                    ? "var(--mainGray)"
+                    : "var(--lightGreen)",
+            );
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section2.p")]
+                    .stateValues.hidden,
+            ).eq(numCompleted < 1);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section2.cm")]
+                    .stateValues.hidden,
+            ).eq(numCompleted >= 1);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section3")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section3")]
+                    .stateValues.hideChildren,
+            ).eq(numCompleted < 2);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section3")]
+                    .stateValues.titleColor,
+            ).eq(
+                stateVariables[await resolvePathToNodeIdx("section3")]
+                    .stateValues.creditAchieved < 1
+                    ? "var(--mainGray)"
+                    : "var(--lightGreen)",
+            );
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section3.p")]
+                    .stateValues.hidden,
+            ).eq(numCompleted < 2);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("section3.cm")]
+                    .stateValues.hidden,
+            ).eq(numCompleted >= 2);
+        }
+
+        let stateVariables = await core.returnAllStateVariables(false, true);
+        const answer11Idx = await resolvePathToNodeIdx("section1.p1.ans");
+        const mathInput11Idx =
+            stateVariables[answer11Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer12Idx = await resolvePathToNodeIdx("section1.p2.ans");
+        const mathInput12Idx =
+            stateVariables[answer12Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer2Idx = await resolvePathToNodeIdx("section2.p.ans");
+        const mathInput2Idx =
+            stateVariables[answer2Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer3Idx = await resolvePathToNodeIdx("section3.p.ans");
+        const mathInput3Idx =
+            stateVariables[answer3Idx].stateValues.inputChildren[0]
+                .componentIdx;
+
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "3",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput3Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer3Idx, core });
+        await check_values(3);
+
+        await updateMathInputValue({
+            latex: "11",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(3);
+    });
+
+    it("one continuation message inside cascade", async () => {
+        let { core, resolvePathToNodeIdx } = await createTestCore({
+            doenetML: `
+<cascade name="w">
+  <title>My cascade</title>
+
+  <div name="div1">
+    <p name="p1a">What is 1+1? <answer name="ans">2</answer></p>
+    <p name="p1b">What is 1-1? <answer name="ans">0</answer></p>
+  </div>
+
+
+  <p name="p2">What is 3+4? <answer name="ans">7</answer></p>
+
+  <p name="p3">What is 3-4? <answer name="ans">-1</answer></p>
+
+  <continuationMessage name="cm"><em>Continued...</em></continuationMessage>
+
+</cascade>
+
+  `,
+        });
+
+        async function check_values(numCompleted: number) {
+            const stateVariables = await core.returnAllStateVariables(
+                false,
+                true,
+            );
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("w")].stateValues
+                    .numCompleted,
+            ).eq(numCompleted);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1")].stateValues
+                    .hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1.p1a")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1.p1b")]
+                    .stateValues.hidden,
+            ).eq(false);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("p2")].stateValues
+                    .hidden,
+            ).eq(numCompleted < 1);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("p3")].stateValues
+                    .hidden,
+            ).eq(numCompleted < 2);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("cm")].stateValues
+                    .hidden,
+            ).eq(numCompleted >= 2);
+        }
+
+        let stateVariables = await core.returnAllStateVariables(false, true);
+        const answer11Idx = await resolvePathToNodeIdx("p1a.ans");
+        const mathInput11Idx =
+            stateVariables[answer11Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer12Idx = await resolvePathToNodeIdx("p1b.ans");
+        const mathInput12Idx =
+            stateVariables[answer12Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer2Idx = await resolvePathToNodeIdx("p2.ans");
+        const mathInput2Idx =
+            stateVariables[answer2Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer3Idx = await resolvePathToNodeIdx("p3.ans");
+        const mathInput3Idx =
+            stateVariables[answer3Idx].stateValues.inputChildren[0]
+                .componentIdx;
+
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "3",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput3Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer3Idx, core });
+        await check_values(3);
+
+        await updateMathInputValue({
+            latex: "11",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(3);
+    });
+
+    it("multiple continuation messages inside cascade", async () => {
+        let { core, resolvePathToNodeIdx } = await createTestCore({
+            doenetML: `
+<cascade name="w">
+  <title>My cascade</title>
+
+  <continuationMessage name="cm1"><em>Never seen</em></continuationMessage>
+
+  <div name="div1">
+    <p name="p1a">What is 1+1? <answer name="ans">2</answer></p>
+    <p name="p1b">What is 1-1? <answer name="ans">0</answer></p>
+  </div>
+
+  <continuationMessage name="cm2"><em>Keep going...</em></continuationMessage>
+
+  <p name="p2">What is 3+4? <answer name="ans">7</answer></p>
+  <continuationMessage name="cm3"><em>Almost done...</em></continuationMessage>
+
+  <p name="p3">What is 3-4? <answer name="ans">-1</answer></p>
+
+  <continuationMessage name="cm4"><em>Never seen either</em></continuationMessage>
+
+</cascade>
+
+  `,
+        });
+
+        async function check_values(numCompleted: number) {
+            const stateVariables = await core.returnAllStateVariables(
+                false,
+                true,
+            );
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("w")].stateValues
+                    .numCompleted,
+            ).eq(numCompleted);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1")].stateValues
+                    .hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1.p1a")]
+                    .stateValues.hidden,
+            ).eq(false);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("div1.p1b")]
+                    .stateValues.hidden,
+            ).eq(false);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("p2")].stateValues
+                    .hidden,
+            ).eq(numCompleted < 1);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("p3")].stateValues
+                    .hidden,
+            ).eq(numCompleted < 2);
+
+            expect(
+                stateVariables[await resolvePathToNodeIdx("cm1")].stateValues
+                    .hidden,
+            ).eq(true);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("cm2")].stateValues
+                    .hidden,
+            ).eq(numCompleted > 0);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("cm3")].stateValues
+                    .hidden,
+            ).eq(numCompleted !== 1);
+            expect(
+                stateVariables[await resolvePathToNodeIdx("cm4")].stateValues
+                    .hidden,
+            ).eq(true);
+        }
+
+        let stateVariables = await core.returnAllStateVariables(false, true);
+        const answer11Idx = await resolvePathToNodeIdx("p1a.ans");
+        const mathInput11Idx =
+            stateVariables[answer11Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer12Idx = await resolvePathToNodeIdx("p1b.ans");
+        const mathInput12Idx =
+            stateVariables[answer12Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer2Idx = await resolvePathToNodeIdx("p2.ans");
+        const mathInput2Idx =
+            stateVariables[answer2Idx].stateValues.inputChildren[0]
+                .componentIdx;
+        const answer3Idx = await resolvePathToNodeIdx("p3.ans");
+        const mathInput3Idx =
+            stateVariables[answer3Idx].stateValues.inputChildren[0]
+                .componentIdx;
+
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "3",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "2",
+            componentIdx: mathInput11Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer11Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(0);
+
+        await updateMathInputValue({
+            latex: "0",
+            componentIdx: mathInput12Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer12Idx, core });
+        await check_values(2);
+
+        await updateMathInputValue({
+            latex: "-1",
+            componentIdx: mathInput3Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer3Idx, core });
+        await check_values(3);
+
+        await updateMathInputValue({
+            latex: "11",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(1);
+
+        await updateMathInputValue({
+            latex: "7",
+            componentIdx: mathInput2Idx,
+            core,
+        });
+        await submitAnswer({ componentIdx: answer2Idx, core });
+        await check_values(3);
+    });
 });
