6fb4d45d210fbaac35f2cd3ad1d2a87e04ed1b27
Duane Nykamp <nykamp@umn.edu>
2025-11-28 19:57:41 -0600

    skip youtube tests in CI for now

--- packages/test-cypress/cypress.config.js
+++ packages/test-cypress/cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     numTestsKeptInMemory: 5,
     defaultCommandTimeout: 30000,
     e2e: {
-        // 1. vital for iframes like YouTube
+        // vital for iframes like YouTube
         chromeWebSecurity: false,
 
         setupNodeEvents(on, config) {
@@ -15,15 +15,15 @@ export default defineConfig({
                     browser.family === "chromium" ||
                     browser.name === "electron"
                 ) {
-                    // 2. Allow autoplay without user interaction
+                    // Allow autoplay without user interaction
                     launchOptions.args.push(
                         "--autoplay-policy=no-user-gesture-required",
                     );
 
-                    // Optional: Mute audio to avoid any hardware/driver issues in CI
+                    // Mute audio to avoid any hardware/driver issues in CI
                     launchOptions.args.push("--mute-audio");
 
-                    // Optional: Fake UI for media usually helps with permissions
+                    // Fake UI for media usually helps with permissions
                     launchOptions.args.push("--use-fake-ui-for-media-stream");
                 }
 
