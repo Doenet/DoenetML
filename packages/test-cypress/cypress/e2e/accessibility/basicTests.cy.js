9f60562975eb10cb8d733d02c8fbb973d49b93a1
Duane Nykamp <nykamp@umn.edu>
2025-12-06 09:38:31 -0600

    some basic accessibility tests

--- packages/test-cypress/cypress/e2e/accessibility/basicTests.cy.js
+++ packages/test-cypress/cypress/e2e/accessibility/basicTests.cy.js
@@ -23,6 +23,104 @@ describe("Render commas tests", function () {
 
         cy.get("#p").should("contain.text", "Hello!");
 
-        cy.checkAccessibility();
+        cy.checkAccessibility(null, {
+            onlyWarnImpacts: ["moderate", "minor"],
+        });
+    });
+
+    it("Sections", () => {
+        cy.window().then(async (win) => {
+            win.postMessage(
+                {
+                    doenetML: `
+                    <title name="title">This document</title>
+                    <section>
+                        <title>My first section</title>
+
+                        <p>A paragraph</p>
+
+                        <section>
+
+                            <p>No title in this sub-section.</p>
+                        </section>
+
+                        <section>
+                            <title>Another sub-section</title>
+
+                            <section>
+                                <title>Next further</title>
+
+                                Hello from here, no paragraph.
+
+                            </section>
+
+                            <section>
+                                <p>This sub-sub-section has no title</p>
+                            </section>
+
+                            <p>Extra text in sub-section</p>
+                        </section>
+
+                        End for first section
+                    </section>
+
+                    <section>
+                        <span>One more thing</span>
+                    </section>
+
+                    <div>Bye</div>
+  `,
+                },
+                "*",
+            );
+        });
+
+        cy.get("#title").should("contain.text", "This document");
+
+        cy.checkAccessibility([".doenet-viewer"], {
+            onlyWarnImpacts: ["moderate", "minor"],
+        });
+    });
+
+    it("Lists", () => {
+        cy.window().then(async (win) => {
+            win.postMessage(
+                {
+                    doenetML: `
+                    <ol>
+                        <li>apple</li>
+                        <li><p>banana</p></li>
+                        <li><ul>
+                            <li>alpha</li>
+                            <li>beta</li>
+                            <li><ol>
+                                <li><p>one</p></li>
+                                <li><p>two</p></li>
+                            </ol></li>
+                            <li>gamma</li>
+                        </ul></li>
+                        <li>cherry</li>
+                    </ol>
+
+                    <ul>
+                        <li>cat</li>
+                        <li><p>dog</p>
+                            <ol>
+                                <li>walk</li>
+                                <li>run</li>
+                            </ol>
+                        </li>
+                    </ul>
+  `,
+                },
+                "*",
+            );
+        });
+
+        cy.get(".doenet-viewer").should("contain.text", "apple");
+
+        cy.checkAccessibility([".doenet-viewer"], {
+            onlyWarnImpacts: ["moderate", "minor"],
+        });
     });
 });
