b90e259d4fab5c912ac7bcdf1298d2aa6492ea24
Duane Nykamp <nykamp@umn.edu>
2026-02-08 01:36:22 -0600

    address review

--- packages/doenetml-worker-javascript/src/utils/checkEquality.js
+++ packages/doenetml-worker-javascript/src/utils/checkEquality.js
@@ -108,9 +108,6 @@ export default function checkEquality({
                 // only if allowing rounding, do we replace constants with floats
                 x = x.constants_to_floats();
             }
-            if (effectiveSimplify === "normalizeorder") {
-                x = x.default_order();
-            }
         } else if (effectiveSimplify === "numberspreserveorder") {
             x = x.evaluate_numbers({
                 max_digits: Infinity,
