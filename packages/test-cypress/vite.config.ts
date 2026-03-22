f858bb15c09feaf37a8b7245398358aa48bb6234
Duane Nykamp <nykamp@umn.edu>
2026-03-22 17:40:50 -0500

    Avoid static-copy ENOENT races in test-cypress

--- packages/test-cypress/vite.config.ts
+++ packages/test-cypress/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
                         "../*",
                     ),
                     dest: "doenetml-worker/",
+                    overwrite: false,
                 },
                 {
                     src: path.join(
@@ -25,6 +26,7 @@ export default defineConfig({
                         "../fonts/*",
                     ),
                     dest: "fonts/",
+                    overwrite: false,
                 },
             ],
         }),
