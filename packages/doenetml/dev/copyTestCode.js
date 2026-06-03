eb2d9f4b443453060f99454eae4e795bf4d11b6c
Steven Clontz <steven.clontz@gmail.com>
2026-06-03 20:24:30 +0000

    make prettier

--- packages/doenetml/dev/copyTestCode.js
+++ packages/doenetml/dev/copyTestCode.js
@@ -1,10 +1,14 @@
-import fs from 'fs'
+import fs from "fs";
 
 // File dev/testCode.doenet will be created if it does not already exist.
-fs.access('dev/testCode.doenet', fs.constants.F_OK, (fileNotFoundErr) => {
+fs.access("dev/testCode.doenet", fs.constants.F_OK, (fileNotFoundErr) => {
     if (fileNotFoundErr) {
-        fs.copyFile('dev/defaultTestCode.doenet', 'dev/testCode.doenet', (copyErr) => {
-            if (copyErr) throw copyErr;
-        });
+        fs.copyFile(
+            "dev/defaultTestCode.doenet",
+            "dev/testCode.doenet",
+            (copyErr) => {
+                if (copyErr) throw copyErr;
+            },
+        );
     }
 });
