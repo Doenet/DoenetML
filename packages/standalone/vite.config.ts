d4d12e1e8ecbf360100869a6b7e86c23a7815214
Jason Siefken <siefkenj@gmail.com>
2026-07-07 19:21:52 +0000

    Autosave doenetml source in dev environments

--- packages/standalone/vite.config.ts
+++ packages/standalone/vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
         }),
         suppressLogPlugin(),
     ],
+    server: {
+        host: "0.0.0.0",
+    },
     build: {
         minify: true,
         sourcemap: true,
