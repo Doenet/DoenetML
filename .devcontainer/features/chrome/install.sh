0ac22378c97aafe77f18ed1470b8b7471342b08b
Kris Hollingsworth <kristhenerd@gmail.com>
2026-08-04 19:26:36 +0000

    Add feature to install chrome.

---
+++ .devcontainer/features/chrome/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+apt-get update
+
+apt-get install -y \
+  wget \
+  gnupg \
+  ca-certificates
+
+mkdir -p /etc/apt/keyrings
+
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub \
+  | gpg --dearmor \
+  > /etc/apt/keyrings/google-chrome.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main" \
+  > /etc/apt/sources.list.d/google-chrome.list
+
+apt-get update
+
+apt-get install -y google-chrome-stable
+
+google-chrome --version
